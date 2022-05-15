e1c010231d24338e34174bd64dbb6737079c652a
Camberden <42500247+Camberden@users.noreply.github.com>
2022-05-15 04:02:58 -0400

    Attempting to enable all CRUD operations

--- src/main/java/com/chrispy/controller/AlienController.java
+++ src/main/java/com/chrispy/controller/AlienController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,57 +27,50 @@ public class AlienController {
 	@Autowired
 	AlienRepo repo;
 	
+	// HOME
 	@RequestMapping("/")
-	public String home() {
-		return "home.jsp";
+	public ModelAndView home() {
+		ModelAndView mv = new ModelAndView("home");
+		return mv;
 	}
 	
-//	@RequestMapping("/addAlien")
-//	public String addAlien(Alien alien) {
-//		repo.save(alien);
-//		return "home.jsp";
-//	}
-	
-//	@RequestMapping("/getAlien")
-//	public ModelAndView getAlien(@RequestParam int aid) {
-//		ModelAndView mv = new ModelAndView("showAlien.jsp");
-//		Alien alien = repo.findById(aid).orElse(new Alien());
-//		mv.addObject(alien);
-//		return mv;
-//	}
-	
-	// CREATE
-	@PostMapping(path="/alien")
-	public Alien addAlien(@RequestBody Alien alien) {
+	// CREATE (POST)
+	@PostMapping
+	public Alien createAlien(@RequestBody Alien alien) {
 		repo.save(alien);
 		return alien;
 	}
 	
 	// READ
-	@GetMapping(path="/aliens")
-	public List<Alien> getAliens() {
+	@RequestMapping("/readAlien")
+	public Alien readAlien(int aid) {
+		Alien alien = repo.findById(aid).orElse(new Alien());
+		home().addObject(alien);
+		return alien;
+	}
+
+	@GetMapping("/aliens")
+	public List<Alien> getAllAliens() {
 		return repo.findAll();
 	}
 		
-	//UPDATE
-	@PutMapping(path="/alien",consumes= {"application/json"})
-	public Alien saveOrUpdateAlien(@RequestBody Alien alien) {
+	//UPDATE (PUT)
+	@PutMapping("/alien")
+	public Alien saveOrUpdateAlien(Alien alien) {
 		repo.save(alien);
 		return alien;
 	}
 		
 	// DELETE
-	@DeleteMapping(path="/alien/{aid}")
-	public String deleteAlien(@PathVariable int aid) {
-		Alien a = repo.getOne(aid);
+	@DeleteMapping("/alien/{aid}")
+	public String deleteAlien(@PathVariable("aid") int aid) {
+		Alien a = repo.getById(aid);
 		repo.delete(a);
 		return "deleted";
 	}
 	
-	
 	@RequestMapping("/alien/{aid}")
-	@ResponseBody
-	public Optional<Alien> getAlien(@PathVariable("aid") int aid) {
+	public Optional<Alien> getAlienById(@PathVariable("aid") int aid) {
 		return repo.findById(aid);
 	}
 	
--- src/main/webapp/home.jsp
+++ src/main/webapp/home.jsp
@@ -4,23 +4,55 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<link href='https://fonts.googleapis.com/css?family=Lora:400italic'
+	rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css" href="styles.css" media="screen" />
+
+
+<title>Spring Boot Testing Ground!</title>
 </head>
 <body>
 
-<p>Submit new Alien data!</p>
-	<form action="addAlien">
+	<h2>Spring Boot Testing Ground!</h2>
+	<h4>Following guide by Telusko</h4>
+
+	<p>Submit new Alien data!</p>
+	<form action="alien">
 		<input type="text" name="aid"><br> 
 		<input type="text" name="aname"><br> 
-			<input type="submit"><br>
-	</form>
-
-<p>Search Alien based on ID</p>
-	<form action="getAlien">
-		<input type="text" name="aid"><br> 
+		<input type="text" name="tech"><br>
 		<input type="submit"><br>
-
 	</form>
 
+	<p>Search Alien based on ID</p>
+	<form action="readAlien">
+		<input type="text" name="aid"><br>
+		<input type="submit"><br>
+	</form>
+
+	<p>Update Alien based on ID</p>
+	
+	<form action=saveOrUpdateAlien>
+		<input type="text" name="aid"><br>
+		<input type="text" name="aname"><br> 
+		<input type="text" name="tech"><br>
+		<input type="submit"><br>
+	</form>
+	
+	<p>Delete Alien based on ID</p>
+	
+	<form action="deleteAlien">
+		<input type="text" name="aid"><br>
+		<input type="submit"><br>
+	</form>
+
+	<p>Let's put some data here!</p>
+	${alien}
+	
+	<form action="getAllAliens">	
+	<input type="button">
+	</form>
+
+<script="action.js"></script>
 </body>
 </html>
